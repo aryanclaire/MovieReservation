97f087ca6165aa9d65dbbe8eab8e004601e1b697
MarvicLazo <lazomarvic.aws@gmail.com>
2024-02-06 17:25:50 +0800

    Added file Details.css

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import './Details.css'
 
 export default function Details() {
   return (
-    <div>Details</div>
+    <div className='container1'>
+        <div className='container2'>
+            <div className='container3'>
+                <p>
+                    asdkjfniaunkajnsd abivbaskjdvnfcoasj
+                </p>
+            </div>
+
+            <div className='container3'>
+                <p>
+                    asdkjfniaunkajnsd abivbaskjdvnfcoasj
+                </p>
+            </div>
+        </div>
+    </div>
   )
 }
