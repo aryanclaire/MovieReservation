e6c3680fd3317c7dc94a2257a5300cf09676104c
MarvicLazo <lazomarvic.aws@gmail.com>
2024-02-08 08:59:36 +0800

    Modified Details design, not yet responsive

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -5,15 +5,35 @@ export default function Details() {
   return (
     <div className='container1'>
         <div className='container2'>
-            <div className='container3'>
+            <div className='reservation'>
+                <header>
+                    <h3>Reservation Description</h3>
+                    
+                </header>
+                
                 <p>
-                    asdkjfniaunkajnsd abivbaskjdvnfcoasj
+                    
                 </p>
             </div>
 
-            <div className='container3'>
+            <div className='seat'>
+                <div>
+                    <h3>Seat Reserved</h3>
+                </div>
+                <div>
+                    <p><small>Total No. of Seats Reserved: 10</small></p>
+                </div>               
                 <p>
-                    asdkjfniaunkajnsd abivbaskjdvnfcoasj
+                    
+                </p>
+            </div>
+
+            <div className='payment'>
+                <header>
+                    <h3>Payment Breakdown</h3>
+                </header>                
+                <p>
+                    
                 </p>
             </div>
         </div>
