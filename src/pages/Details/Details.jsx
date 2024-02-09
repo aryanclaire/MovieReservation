aafd6f1504b3dbd7e1c0de0b9d0103751b6aa22d
MarvicLazo <lazomarvic.aws@gmail.com>
2024-02-09 15:08:58 +0800

    Seats Reserved 90% done

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -6,26 +6,48 @@ export default function Details() {
     <div className='container1'>
         <div className='container2'>
             <div className='reservation'>
-                <header>
-                    <h3>Reservation Description</h3>
-                    
-                </header>
-                
+                <div>
+                <h3>Reservation Description</h3>
+                </div>
+                <div className='smallText'>
+                    <p>Total No. of Seats Reserved: 10</p>
+                </div>                
                 <p>
                     
                 </p>
             </div>
 
             <div className='seat'>
-                <div>
+                <div style={{margin: '0 0 0 0'}}>
                     <h3>Seat Reserved</h3>
                 </div>
                 <div className='smallText'>
                     <p>Total No. of Seats Reserved: 10</p>
                 </div>               
+                <h4>
+                    SEAT ID
+                </h4>
+                <h4 className='secondColumn'>
+                    PRICE
+                </h4>
                 <p>
-                    
+                    asdasdasd
                 </p>
+                <p className='secondColumn'>
+                    asdasdasd
+                </p>
+                <p>
+                    asdasdasd
+                </p>
+                <p className='secondColumn'>
+                    asdasdasd
+                </p>
+                <p>
+                     
+                </p>
+                <h4 className='total'>
+                    TOTAL: 1,400
+                </h4>
             </div>
 
             <div className='payment'>
