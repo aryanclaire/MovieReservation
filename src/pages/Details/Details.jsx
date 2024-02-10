464fe551e5cd0ffd5d55f4e29b3cad469b9ae051
katiiiiiiii <balsomokathy.aws@gmail.com>
2024-02-10 17:30:20 +0800

    new updates

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,52 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Details.css'
-import { Box, TextField } from '@mui/material'
+import { Box, Checkbox, TextField } from '@mui/material'
 
 export default function Details() {
-  return (
-    <div className='container1'>
-        <div className='container2'>
-            <div className='reservation'>
-                <header>
+    const [checked, setChecked] = useState(true);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+      };
+
+    return (
+        <div className='container1'>
+            <div className='container2'>
+                <div className='reservation'>
                     <h3>Reservation Description</h3>
-                </header>
-                <div className='textfield'>
-                    <Box
-                        component="form"
-                        sx={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            '& > :not(style)': { m: 1, width: '19ch' },
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <TextField id="fname" label="First Name" variant="outlined" />
-                        <TextField id="mname" label="Middle Name" variant="outlined" />
-                        <TextField id="lname" label="Last Name" variant="outlined" />
-                    </Box>
-                </div>
-            </div>
-
-            <div className='seat'>
-                <div>
-                    <h3>Seat Reserved</h3>
-                </div>
-                <div className='smallText'>
-                    <p>Total No. of Seats Reserved: 10</p>
-                </div>               
-        
-            </div>
-
-            <div className='payment'>
-                <header>
-                    <h3>Payment Breakdown</h3>
-                </header>                
-                <p>
+                    <h4>Customer Description</h4>
+                    <div className='textfield'>
+                        <Box
+                            component="form"
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                '& > :not(style)': { m: 1, width: '19ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <TextField id="fname" label="First Name" variant="outlined" />
+                            <TextField id="mname" label="Middle Name" variant="outlined" />
+                            <TextField id="lname" label="Last Name" variant="outlined" />
+                        </Box>
+                    </div>
                     
-                </p>
+                    <div className='checkbox'>
+                        <Checkbox
+                            checked={checked}
+                            onChange={handleChange}
+                            inputProps={{ 'aria-label': 'controlled' }}
+                        /> <p>Senior Citizen</p>
+                    </div>
+                    
+                    <h4>Movie Description</h4>
+                    <div className='movie'>
+                        <p>Movie Title: Rewind</p>
+                        <p>Type: Regular</p>
+                        <p>Date: FEB 02, 2024</p>
+                        <p>Start Time: 12:00 pm</p>
+                        <p>End Time: 2:30 pm</p>
+                        <p>No. of Hours: 2 hrs 30 min</p>
+                        <p>Price: 350.00</p>
+                        <p>MPA Final Rating: Rated G</p>
+                    </div>
+                </div>
+
+
+
+                
+
+                <div className='seat'>
+                    <div>
+                        <h3>Seat Reserved</h3>
+                    </div>
+                    <div className='smallText'>
+                        <p>Total No. of Seats Reserved: 10</p>
+                    </div>               
+            
+                </div>
+
+                <div className='payment'>
+                    <header>
+                        <h3>Payment Breakdown</h3>
+                    </header>                
+                    <p>
+                        
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
