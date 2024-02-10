00c49f635872acd1cc1458d5a127780d983c185d
MarvicLazo <lazomarvic.aws@gmail.com>
2024-02-10 14:46:59 +0800

    Reservation Details 90% done

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -6,48 +6,54 @@ export default function Details() {
     <div className='container1'>
         <div className='container2'>
             <div className='reservation'>
-                <div>
-                <h3>Reservation Description</h3>
+                <div className='reservation2'>
+                    <div>
+                        <h3>Reservation Description</h3>
+                    </div>
+                    <div className='smallText'>
+                        <p>ID: (id here)</p>
+                    </div>
                 </div>
-                <div className='smallText'>
-                    <p>ID: asdifhoainjh</p>
-                </div>                
-                <p>
-                    
-                </p>
+                <p><b>Customer Description</b></p>
+                <div className='customerDescription'>
+                    <p>First Name: (type here)</p>
+                    <p>Last Name: (date here)</p>
+                    <p>Middle Time: (time here)</p>
+                    <p>(input box here)</p>
+                    <p>(input box here)</p>
+                    <p>(input box here)</p>
+                    <p>(checkbox here) Senior Citizen</p>
+                </div>
+                <p><b>Movie Description</b></p>
+                <div className='movieDescription'>
+                    <p>Movie Title: (title here)</p>
+                    <p></p>
+                    <p>Type: (type here)</p>
+                    <p>Date: (date here)</p>
+                    <p>Start Time: (time here)</p>
+                    <p>End Time: (time here)</p>
+                    <p>Duration: (duration here)</p>
+                    <p>Price: (price here)</p>
+                    <p></p>
+                    <p>MPA FILM RATING: (rating here)</p>
+                </div>
             </div>
 
             <div className='seat'>
-                <div style={{margin: '0 0 0 0'}}>
+                <div>
                     <h3>Seat Reserved</h3>
                 </div>
                 <div className='smallText'>
-                    <p>Total No. of Seats Reserved: 10</p>
-                </div>               
-                <h4>
-                    SEAT ID
-                </h4>
-                <h4 className='secondColumn'>
-                    PRICE
-                </h4>
-                <p>
-                    asdasdasd
-                </p>
-                <p className='secondColumn'>
-                    asdasdasd
-                </p>
-                <p>
-                    asdasdasd
-                </p>
-                <p className='secondColumn'>
-                    asdasdasd
-                </p>
-                <p>
-                     
-                </p>
-                <h4 className='total'>
-                    TOTAL: 1,400
-                </h4>
+                    <p>Total No. of Seats Reserved: (seats here)</p>
+                </div>
+                <h4> SEAT ID</h4>
+                <h4 className='secondColumn'>PRICE</h4>
+                <p>(ID1 here)</p>
+                <p className='secondColumn'>(price here)</p>
+                <p>(ID2 here)</p>
+                <p className='secondColumn'>(price here)</p>
+                <p></p>
+                <h4 className='total'>TOTAL: (total here)</h4>
             </div>
 
             <div className='payment'>
@@ -55,30 +61,14 @@ export default function Details() {
                     <h3>Payment Breakdown</h3>
                 </header>
                 <div className='payment2'>
-                    <p><b>
-                        Type:
-                    </b></p>
-                    <p className='secondColumn'>
-                        350(REGULAR)
-                    </p>
-                    <p><b>
-                        Number of Seats:
-                    </b></p>
-                    <p className='secondColumn'>
-                        4
-                    </p>
-                    <p><b>
-                        Discount:
-                    </b></p>
-                    <p className='secondColumn'>
-                        20%
-                    </p>
-                    <p>
-                        
-                    </p>
-                    <h4 className='total'>
-                        AMOUNT OF PAY: 1,120
-                    </h4>
+                    <p><b>Type:</b></p>
+                    <p className='secondColumn'>(type here)</p>
+                    <p><b>Number of Seats:</b></p>
+                    <p className='secondColumn'>(number here)</p>
+                    <p><b>Discount:</b></p>
+                    <p className='secondColumn'>(discount here)</p>
+                    <p></p>
+                    <h4 className='total'>AMOUNT OF PAY: (total here)</h4>
                 </div>             
             </div>
         </div>
